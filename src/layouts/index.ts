141aad6c9150f834cecbaa1f7a79c142544ac9c7
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-10 18:21:52 +0000

    feat: add PageWithLayout typing

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,8 +1,11 @@
+import type { NextPage } from "next";
 import type { PropsWithChildren } from "react";
 
 export type WithLayout<T> = T & {
   Layout: ({ children }: PropsWithChildren) => JSX.Element;
 };
+
+export type PageWithLayout = WithLayout<NextPage>;
 
 export function hasLayout<T>(Component: T): Component is WithLayout<T> {
   return "layout" in Component;
