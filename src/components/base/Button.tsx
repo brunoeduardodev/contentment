4c847ade97cbd9c2cc1e6da2911f210967253c30
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-10 19:26:34 +0000

    feat: add Button and LinkButton

---
+++ src/components/base/Button.tsx
@@ -0,0 +1,63 @@
+import type { VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
+import Link from "next/link";
+import type { ComponentProps } from "react";
+import type { IconType } from "react-icons";
+
+const buttonStyles = cva(
+  "text-white flex items-center outline-white focus-visible:outline-1 transition-colors duration-300 ease-in-out disabled:bg-black/10 focus-visible:bg-white/10 hover:bg-white/10 active:bg-white/20",
+  {
+    variants: {
+      size: {
+        sm: "px-4 py-3 rounded gap-1 text-sm",
+        md: "px-5 py-4 rounded gap-2 text-md",
+        lg: "px-6 py-4 rounded gap-3 text-lg",
+      },
+    },
+  }
+);
+
+const iconStyles = cva("text-white", {
+  variants: {
+    size: {
+      sm: "w-4 h-4",
+      md: "w-5 h-5",
+      lg: "w-6 h-6",
+    },
+  },
+});
+
+type Props = {
+  LeftIcon: IconType;
+} & ComponentProps<"button"> &
+  VariantProps<typeof buttonStyles>;
+
+export const Button = ({ LeftIcon, size, className, children }: Props) => {
+  return (
+    <button className={`${buttonStyles({ size })} ${className}`}>
+      <LeftIcon className={iconStyles({ size })} />
+      {children}
+    </button>
+  );
+};
+
+type LinkButtonProps = {
+  LeftIcon: IconType;
+  href: string;
+} & ComponentProps<typeof Link> &
+  VariantProps<typeof buttonStyles>;
+
+export const LinkButton = ({
+  LeftIcon,
+  size,
+  children,
+  className,
+  href,
+}: LinkButtonProps) => {
+  return (
+    <Link href={href} className={`${buttonStyles({ size })} ${className}`}>
+      <LeftIcon className={iconStyles({ size })} />
+      {children}
+    </Link>
+  );
+};
