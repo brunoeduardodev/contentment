9b80a4b7b474b9421aa7972aaa04e63393b0fd54
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-11 15:33:14 +0000

    feat: add posts router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,8 +1,10 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
+import { postsRouter } from "./posts";
 
 export const appRouter = router({
   auth: authRouter,
+  posts: postsRouter,
 });
 
 // export type definition of API
