776bb240ace3700cf4b4ab1231e27763f09cf6bf
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-11 16:42:24 +0000

    feat: add get posts route

--- src/server/trpc/router/posts.ts
+++ src/server/trpc/router/posts.ts
@@ -1,3 +1,34 @@
-import { router } from "../trpc";
+import { TRPCError } from "@trpc/server";
+import { getPostsSchema } from "../../schemas/getPosts";
+import { protectedProcedure, router } from "../trpc";
 
-export const postsRouter = router({});
+export const postsRouter = router({
+  get: protectedProcedure
+    .input(getPostsSchema)
+    .query(
+      async ({ input: { authorId, skip, take }, ctx: { prisma, session } }) => {
+        if (session.user.role === "User" && !authorId) {
+          throw new TRPCError({
+            code: "FORBIDDEN",
+          });
+        }
+
+        return prisma.post.findMany({
+          select: {
+            _count: true,
+            author: { select: { name: true, image: true } },
+            title: true,
+            categories: { select: { name: true } },
+            shortDescription: true,
+            createdAt: true,
+            updatedAt: true,
+            slug: true,
+            id: true,
+          },
+          skip,
+          take,
+          where: { authorId },
+        });
+      }
+    ),
+});
