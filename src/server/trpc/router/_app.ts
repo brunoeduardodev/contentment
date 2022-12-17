a64051258bd84f9bdeeadcf2a669258663c2dc72
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-17 08:48:24 +0000

    feat: add categories router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
+import { categoriesRouter } from "./categories";
 import { postsRouter } from "./posts";
 
 export const appRouter = router({
   auth: authRouter,
   posts: postsRouter,
+  categories: categoriesRouter,
 });
 
 // export type definition of API
